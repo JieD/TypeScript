1f6779910fab34cc65c1a9e85cde74f01e173939
jied <jied314@mail.sfsu.edu>
2015-02-11 19:12:39 -0800

    test struct initialization

--- tests/cases/conformance/structs/structInitializations/structInitializedWithWrongSequence.ts
+++ tests/cases/conformance/structs/structInitializations/structInitializedWithWrongSequence.ts
@@ -3,4 +3,5 @@ struct Person {
 }
 var john = new Person("John", 25); // correct
 var george = new Person(45, "George"); // wrong, sequence matters
+var jg = new Person({age: 45, name: "George"}); // wrong, sequence matters
 
