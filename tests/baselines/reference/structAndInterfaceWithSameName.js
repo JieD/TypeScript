c96323970a10f326ffadd716a7698561f4b1662c
jied <jied314@mail.sfsu.edu>
2015-02-11 22:33:59 -0800

    add baselines for struct tests

---
+++ tests/baselines/reference/structAndInterfaceWithSameName.js
@@ -0,0 +1,28 @@
+//// [structAndInterfaceWithSameName.ts]
+struct C { foo: string; }
+interface C { foo: string; } // error
+
+module M {
+    struct D {
+        bar: string;
+    }
+
+    interface D { // error
+        bar: string;
+    }
+}
+
+//// [structAndInterfaceWithSameName.js]
+var C = (function () {
+    function C() {
+    }
+    return C;
+})();
+var M;
+(function (M) {
+    var D = (function () {
+        function D() {
+        }
+        return D;
+    })();
+})(M || (M = {}));
