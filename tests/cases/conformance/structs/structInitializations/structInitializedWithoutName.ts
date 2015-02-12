77574fc41a45991b9e1a4c1374d1384f71cbaa4a
jied <jied314@mail.sfsu.edu>
2015-02-11 19:07:18 -0800

    correct errors in test cases

--- tests/cases/conformance/structs/structInitializations/structInitializedWithoutName.ts
+++ tests/cases/conformance/structs/structInitializations/structInitializedWithoutName.ts
@@ -1,7 +1,7 @@
 struct Person {
-	constructor(public name: string, public age: number) {};
+	constructor(public name: string, public age: number) {}
 }
 
 var test = function (person: Person) {}
-test(new Person());
+test(new Person()); // error
 test({ name: "John", age: 25 }); // wrong, need an explicit declaration
--- tests/cases/structs/structInitializations/structInitializedWithWrongSequence.ts
+++ tests/cases/structs/structInitializations/structInitializedWithWrongSequence.ts
@@ -1,5 +1,5 @@
 struct Person {
-	constructor(public name: string, public age: number) {};
+	constructor(public name: string, public age: number) {}
 }
 var john = new Person("John", 25); // correct
 var george = new Person(45, "George"); // wrong, sequence matters
