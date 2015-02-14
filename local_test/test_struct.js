091796975e239e9083d40c64c3ca538596215753
Yanxing Wang <wangya@mail.sfsu.edu>
2015-02-13 18:12:58 -0800

    temp update for builds

--- local_test/test_struct.js
+++ local_test/test_struct.js
@@ -1,10 +1,41 @@
-var Greeter = (function () {
-    function Greeter(message) {
-        this.greeting = message;
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var Animal = (function () {
+    function Animal(name) {
+        this.name = name;
     }
-    Greeter.prototype.greet = function () {
-        return "Hello, " + this.greeting;
+    Animal.prototype.move = function (meters) {
+        console.log(this.name + " moved " + meters + "m.");
     };
-    return Greeter;
+    return Animal;
 })();
-var greeter = new Greeter("world");
+var Snake = (function (_super) {
+    __extends(Snake, _super);
+    function Snake(name) {
+        _super.call(this, name);
+    }
+    Snake.prototype.move = function () {
+        console.log("Slithering...");
+        console.log(5);
+    };
+    return Snake;
+})(Animal);
+var Horse = (function (_super) {
+    __extends(Horse, _super);
+    function Horse(name) {
+        _super.call(this, name);
+    }
+    Horse.prototype.move = function () {
+        console.log("Galloping...");
+        console.log(45);
+    };
+    return Horse;
+})(Animal);
+var sam = new Snake("Sammy the Python");
+var tom = new Horse("Tommy the Palomino");
+sam.move();
+tom.move(34);
--- local_test/test_struct.ts
+++ local_test/test_struct.ts
@@ -1,11 +1,28 @@
-struct Greeter {
-    greeting: string;
-    constructor(message: string) {
-        this.greeting = message;
-    }
-    greet() {
-        return "Hello, " + this.greeting;
+class Animal {
+    constructor(public name: string) { }
+    move(meters: number) {
+        console.log(this.name + " moved " + meters + "m.");
     }
 }
 
-var greeter = new Greeter("world");
+class Snake extends Animal {
+    constructor(name: string) { super(name); }
+    move() {
+        console.log("Slithering...");
+        console.log(5);
+    }
+}
+
+class Horse extends Animal {
+    constructor(name: string) { super(name); }
+    move() {
+        console.log("Galloping...");
+        console.log(45);
+    }
+}
+
+var sam = new Snake("Sammy the Python");
+var tom: Animal = new Horse("Tommy the Palomino");
+
+sam.move();
+tom.move(34);
