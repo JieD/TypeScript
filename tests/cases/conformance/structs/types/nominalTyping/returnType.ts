f305983c8b445d10bad037660df499942fe1ea7a
jied <jied314@mail.sfsu.edu>
2015-02-15 21:02:02 -0800

    type compatability tests

---
+++ tests/cases/conformance/structs/types/nominalTyping/returnType.ts
@@ -0,0 +1,9 @@
+class Person {
+	constructor(public name: string, public age: number) {}
+}
+
+var p: Person;
+var test = (name: string, age: number) => new Person(name, age);
+p = test("John", 25);
+test = function (n, a) {return {name: n, age: a} }; // error, type not compatible
+
