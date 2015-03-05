fd2f249f5b168ff073c783d40b902ffb34dc861a
jied <jied314@mail.sfsu.edu>
2015-03-05 00:20:59 -0800

    change error messages

--- tests/cases/conformance/structs/structDeclarations/structHeritageSpecification/structAppearsToHaveMembersOfObject.ts
+++ tests/cases/conformance/structs/structDeclarations/structHeritageSpecification/structAppearsToHaveMembersOfObject.ts
@@ -4,4 +4,4 @@ var c: C;
 var r = c.toString(); // call members of JS Object
 var r2 = c.hasOwnProperty('');
 var o: Object = c; // error, not assignable
-var o2: {} = c;
+var o2: {} = c; // error, not assignable
