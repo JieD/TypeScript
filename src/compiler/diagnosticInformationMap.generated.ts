c8f0196470123de0054de341ecbf4a93fad76743
jied <jied314@mail.sfsu.edu>
2015-03-04 19:11:20 -0800

    change error message

--- src/compiler/diagnosticInformationMap.generated.ts
+++ src/compiler/diagnosticInformationMap.generated.ts
@@ -407,7 +407,7 @@ module ts {
         _0_modifier_cannot_appear_on_a_struct_element: { code: 8004, category: DiagnosticCategory.Error, key: "'{0}' modifier cannot appear on a struct element." },
 	    Struct_0_incorrectly_extends_base_struct_1: { code: 8005, category: DiagnosticCategory.Error, key: "Struct '{0}' incorrectly extends base struct '{1}'." },
 	    Struct_0_incorrectly_extends_base_struct_1_Colon: { code: 8006, category: DiagnosticCategory.Error, key: "Struct '{0}' incorrectly extends base struct '{1}':" },
-	    A_struct_can_not_implement_another_class_or_interface: { code: 8007, category: DiagnosticCategory.Error, key: "A struct can not implement another class or interface." },
+	    A_struct_can_not_implement_another_class_or_interface: { code: 8007, category: DiagnosticCategory.Error, key: "A struct can not implement interface." },
 	    Struct_name_cannot_be_0: { code: 8008, category: DiagnosticCategory.Error, key: "Struct name cannot be '{0}'" },
     };
 }
