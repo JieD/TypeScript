7f95db9ec1a217d6f81b3c7e77a8041c9a4c415a
jied <jied314@mail.sfsu.edu>
2015-03-10 16:20:25 -0700

    add error messages

--- tests/cases/conformance/structs/constructorDeclarations/superCalls/derivedStructSuperCallsInNonConstructorMembers.ts
+++ tests/cases/conformance/structs/constructorDeclarations/superCalls/derivedStructSuperCallsInNonConstructorMembers.ts
@@ -1,31 +1,32 @@
-// error to use super calls outside a constructor
+// @target: ES5
+// error to use super calls outside a constructor or in nested functions inside constructors
 
 struct Base {
     x: string;
 }
 
 struct Derived extends Base {
-    a: super();
+    a: super(); // error
     b() {
-        super();
+        super(); // error
     }
     get C() {
-        super();
+        super(); // error
         return 1;
     }
     set C(v) {
-        super();
+        super(); // error
     }
 
-    static a: super();
+    static a: super(); // error
     static b() {
-        super();
+        super(); // error
     }
     static get C() {
-        super();
+        super(); // error
         return 1;
     }
     static set C(v) {
-        super();
+        super(); // error
     }
 }
