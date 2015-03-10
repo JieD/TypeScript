e01b225830fcb7cffb1b3cde498515c7357dc432
jied <jied314@mail.sfsu.edu>
2015-03-10 12:34:16 -0700

    constructor overloads with default values

--- tests/cases/conformance/structs/constructorDeclarations/constructorParameters/structConstructorOverloadsWithDefaultValues.ts
+++ tests/cases/conformance/structs/constructorDeclarations/constructorParameters/structConstructorOverloadsWithDefaultValues.ts
@@ -1,8 +1,17 @@
+// A parameter initializer is only allowed in a function or constructor implementation
+
 struct C {
     foo: string;
     constructor(x = 1); // error
     constructor() {
     }
+}
+
+struct C {
+	foo: string;
+	constructor();
+	constructor(x = 1) { // ok
+	}
 }
 
 struct D<T> {
