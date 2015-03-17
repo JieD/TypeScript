d1e6d3c30b939fa136e1fc7a3f1cf290347ac9e8
jied <jied314@mail.sfsu.edu>
2015-03-16 22:55:20 -0700

    derived type & static member

--- tests/cases/conformance/structs/propertyMemberDeclarations/memberFunctionDeclarations/staticMemberAssignsToStructConstructorFunctionMembers.ts
+++ tests/cases/conformance/structs/propertyMemberDeclarations/memberFunctionDeclarations/staticMemberAssignsToStructConstructorFunctionMembers.ts
@@ -1,10 +1,10 @@
-class C {
+struct C {
     static foo() {
         C.foo = () => { }
     }
 
     static bar(x: number): number {
-        C.bar = () => { } // error
+        C.bar = () => { } // error, Type '() => void' is not assignable to type '(x: number) => number'
         C.bar = (x) => x; // ok
         C.bar = (x: number) => 1; // ok
         return 1;
--- tests/cases/conformance/structs/propertyMemberDeclarations/memberFunctionDeclarations/structStaticFactory1.ts
+++ tests/cases/conformance/structs/propertyMemberDeclarations/memberFunctionDeclarations/structStaticFactory1.ts
@@ -1,11 +1,11 @@
-class Base {
+struct Base {
     foo() { return 1; }
     static create() {
         return new this();
     }
 }
 
-class Derived extends Base {
+struct Derived extends Base {
     foo() { return 2; }
 }
 var d = Derived.create(); 
