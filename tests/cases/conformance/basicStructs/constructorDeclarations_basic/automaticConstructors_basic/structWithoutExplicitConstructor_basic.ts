54548bb205cc8430bcce3ab3aab636146a4c1275
jied <jied314@mail.sfsu.edu>
2015-03-24 16:51:51 -0700

    remove inheritance

--- tests/cases/conformance/basicStructs/constructorDeclarations_basic/automaticConstructors_basic/structWithoutExplicitConstructor_basic.ts
+++ tests/cases/conformance/basicStructs/constructorDeclarations_basic/automaticConstructors_basic/structWithoutExplicitConstructor_basic.ts
@@ -5,11 +5,3 @@ struct C {
 
 var c = new C();
 var c2 = new C(null); // error, Supplied parameters do not match any signature of call target
-
-struct D<T extends Date> {
-    x = 2
-    y: T = null;
-}
-
-var d = new D();
-var d2 = new D(null); // error, Supplied parameters do not match any signature of call target
