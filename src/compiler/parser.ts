aa390c314b5e6b7a5501748f9393af3c36da21d7
jied <jied314@mail.sfsu.edu>
2015-04-15 20:12:18 -0700

    struct not allow index member

--- src/compiler/parser.ts
+++ src/compiler/parser.ts
@@ -3708,14 +3708,17 @@ module ts {
                 return parsePropertyMemberDeclaration(pos, flags);
             }
             if (token === SyntaxKind.OpenBracketToken) {
-                //if (flags) {
-                //    var start = getTokenPos(pos);
-                //    var length = getNodePos() - start;
-                //    errorAtPos(start, length, Diagnostics.Modifiers_not_permitted_on_index_signature_members);
-                //}
-                //return parseIndexSignatureMember();
-	            var start = getTokenPos(pos);
-	            var length = 2;
+	            var start = 0;
+	            var length = 0;
+                if (flags) {
+                    start = getTokenPos(pos);
+                    length = getNodePos() - start;
+                    errorAtPos(start, length, Diagnostics.Modifiers_not_permitted_on_index_signature_members);
+	                start += length + 1;
+                } else {
+	                start = getTokenPos(pos);
+                }
+	            length = 1;
 	            errorAtPos(start, length, Diagnostics.index_member_not_allowed_for_struct);
 	            return parseIndexSignatureMember();
             }
