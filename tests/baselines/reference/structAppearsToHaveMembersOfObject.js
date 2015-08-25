0af1046fb1a7fe480942becdd94978ab04ed4f51
jied <jied314@mail.sfsu.edu>
2015-08-25 14:35:30 -0700

    add baseline - same name and struct contains Object members

---
+++ tests/baselines/reference/structAppearsToHaveMembersOfObject.js
@@ -0,0 +1,21 @@
+//// [structAppearsToHaveMembersOfObject.ts]
+struct C { foo: string; }
+
+var c: C;
+var r = c.toString(); // call members of JS Object
+var r2 = c.hasOwnProperty('');
+var o: Object = c; // error, not assignable
+var o2: {} = c; // error, not assignable
+
+
+//// [structAppearsToHaveMembersOfObject.js]
+var C = (function () {
+    function C() {
+    }
+    return C;
+})();
+var c;
+var r = c.toString(); // call members of JS Object
+var r2 = c.hasOwnProperty('');
+var o = c; // error, not assignable
+var o2 = c; // error, not assignable
