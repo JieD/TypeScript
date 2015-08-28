58ecaf132ad704638a92861efda7b31abcab7aa6
jied <jied314@mail.sfsu.edu>
2015-08-28 14:20:55 -0700

    folder and file rename

--- tests/cases/conformance/structs/members/accessibility/structPrivateStaticMemberAccessibility.ts
+++
@@ -1,8 +0,0 @@
-struct Base {
-    private static foo: string;
-}
-
-struct Derived extends Base {
-    static bar = Base.foo; // error
-    bing = () => Base.foo; // error
-}
--- tests/cases/conformance/structs/members/instanceAndStaticMembers/typeOfThisInStructStaticMembers2.ts
+++
@@ -1,7 +0,0 @@
-struct C {
-    static foo = this; // error, 'this' cannot be referenced in a static property initializer.
-}
-
-struct C2<T> {
-    static foo = this; // error
-}
